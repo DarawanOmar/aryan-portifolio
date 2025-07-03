fd001da5172daacb0507b1625843b693a2c0a165
DarawanOmar <darait721@gmail.com>
2025-07-04 00:18:47 +0300

    add me

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import ProfileSection from "@/components/ProfileSection";
 import { portfolioData } from "./data";
 import { ThemeToggleButton } from "@/components/toggle-mode";
 import { ThemeSelector } from "@/components/theme/theme-selector";
+import Link from "next/link";
 
 function App() {
   return (
@@ -16,16 +17,26 @@ function App() {
       {/* Content */}
       <div className="z-10 py-16 px-4 sm:px-6 lg:px-8 relative">
         <div className="max-w-4xl mx-auto space-y-16 ">
-          <div className="absolute top-4 left-4 ">
+          <div className="fixed top-6 left-6 ">
             <ThemeToggleButton />
           </div>{" "}
-          <div className=" absolute top-4 left-4 ">
+          <div className=" fixed top-6 right-6 ">
             <ThemeSelector />
           </div>{" "}
           {/* Profile Section */}
           <ProfileSection data={portfolioData} />
           <ExperienceSection jobs={portfolioData.jobs} />
           {/* Experience Section */}
+          <div className="max-w-max mx-auto flex gap-3">
+            <p>Created by</p>
+            <Link
+              className="text-primary"
+              href={"https://darawanomar.vercel.app"}
+            >
+              {/* <Link className="text-primary" href={"https://wa.me/9647512813327"}> */}
+              Darawan Omar
+            </Link>
+          </div>
         </div>
       </div>
     </main>
